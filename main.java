1828551728ebc5f5f283f382e009b79ec4d45172
ra191393 <ricardoamaya191393@correo.itm.edu.co>
2024-11-17 19:09:24 -0500

    PRIMERA CARGA

---
+++ main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        // Muestra un saludo en la consola
+        System.out.println("¡Hola, bienvenido a mi proyecto de Java!");
+    }
+}
